2748fcd967207ddc4f7a9a1a4c55d41076a525b3
SavindaJayasekara <thantrige32@gmail.com>
2023-08-22 14:07:27 +0530

    fix navigate to dashbord !

--- src/main/java/lk/ijse/d24hostalmng/controller/LoginFormController.java
+++ src/main/java/lk/ijse/d24hostalmng/controller/LoginFormController.java
@@ -3,21 +3,44 @@ package lk.ijse.d24hostalmng.controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginFormController {
     public JFXTextField txtUserName;
     public JFXPasswordField textPassword;
+    public AnchorPane root;
 
     public void txtPasswordOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnLogInOnAction(MouseEvent event) {
+    public void btnLogInOnAction(MouseEvent event) throws IOException {
+        Parent root = null;
+        root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/dashbord-form.fxml")));
+        if (root != null) {
+            Scene subScene = new Scene(root);
+            Stage primaryStage = (Stage) this.root.getScene().getWindow();
+            primaryStage.setScene(subScene);
+            primaryStage.centerOnScreen();
+
+            TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+            tt.setFromX(-subScene.getWidth());
+            tt.setToX(0);
+            tt.play();
+        }
     }
 
     public void mouseEnterImg(MouseEvent event) {
