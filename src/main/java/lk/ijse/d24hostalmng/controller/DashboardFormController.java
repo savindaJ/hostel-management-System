83953e6f604000690ca83b29aae20b942d412cf7
SavindaJayasekara <thantrige32@gmail.com>
2023-08-22 14:02:47 +0530

    add dashbord !

---
+++ src/main/java/lk/ijse/d24hostalmng/controller/DashboardFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.d24hostalmng.controller;
+
+import javafx.scene.control.Label;
+
+public class DashboardFormController {
+    public Label lblRoomTotal;
+    public Label lblStuTotal;
+    public Label lblAvRoom;
+}
