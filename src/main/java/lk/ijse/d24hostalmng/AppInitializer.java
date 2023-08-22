6cfead4b2a26ac13fc519c0d5a334cfad8446660
SavindaJayasekara <thantrige32@gmail.com>
2023-08-22 11:29:25 +0530

    add launcher !"

---
+++ src/main/java/lk/ijse/d24hostalmng/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse.d24hostalmng;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/lk/ijse/d24hostalmng/Launcher.java
@@ -0,0 +1,7 @@
+package lk.ijse.d24hostalmng;
+
+public class Launcher {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
