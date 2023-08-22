74b40a7a86c6e580a938d04567dcd63a196a86e2
SavindaJayasekara <thantrige32@gmail.com>
2023-08-22 12:29:12 +0530

    fix login form design !

--- src/main/java/lk/ijse/d24hostalmng/controller/LoginFormController.java
+++ src/main/java/lk/ijse/d24hostalmng/controller/LoginFormController.java
@@ -1,4 +1,51 @@
 package lk.ijse.d24hostalmng.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.animation.ScaleTransition;
+import javafx.event.ActionEvent;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
 public class LoginFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField textPassword;
+
+    public void txtPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogInOnAction(MouseEvent event) {
+    }
+
+    public void mouseEnterImg(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(15);
+            glow.setHeight(15);
+            glow.setRadius(15);
+            icon.setEffect(glow);
+        }
+    }
+
+    public void mouseExitImg(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
+        }
+    }
 }
