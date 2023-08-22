593a6f57df1bb28963b1787989e0b67ad41f7536
SavindaJayasekara <thantrige32@gmail.com>
2023-08-22 22:32:55 +0530

    add property file configuration !

--- src/main/java/lk/ijse/d24hostalmng/configuration/Configure.java
+++ src/main/java/lk/ijse/d24hostalmng/configuration/Configure.java
@@ -4,15 +4,25 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Configure {
     private static Configure configure;
     private static final SessionFactory factory;
 
     static {
-        factory = new Configuration()
-                .configure()
-                .addAnnotatedClass(Object.class)
-                .buildSessionFactory();
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.Properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        configuration.addAnnotatedClass(CustomerCopy.class).addAnnotatedClass(Item.class);
+        factory=configuration.setProperties(properties).buildSessionFactory();
     }
 
     private Configure(){}
---
+++ src/main/java/lk/ijse/d24hostalmng/controller/StudentFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.d24hostalmng.controller;
+
+public class StudentFormController {
+}
