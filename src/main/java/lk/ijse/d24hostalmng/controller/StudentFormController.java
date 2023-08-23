14b166264fbca846d9473dccc65a7a43e48eb22b
SavindaJayasekara <thantrige32@gmail.com>
2023-08-23 19:32:02 +0530

    add student controller propertyst and behaviours !

--- src/main/java/lk/ijse/d24hostalmng/controller/StudentFormController.java
+++ src/main/java/lk/ijse/d24hostalmng/controller/StudentFormController.java
@@ -1,5 +1,44 @@
 package lk.ijse.d24hostalmng.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentFormController {
 
+    public JFXTextField txtStudentNic;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentContact;
+    public JFXComboBox<String> cmbGender;
+    public TableView tblStudentDetail;
+    public TableColumn colNIC;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colContact;
+    public TableColumn colDate;
+    public TableColumn colGender;
+    public DatePicker datePicker;
+
+    @FXML
+    void initialize(){
+
+    }
+
+    public void btnAddNewOnAction(ActionEvent event) {
+    }
+
+    public void btnAddOnAction(ActionEvent event) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
