82decd84932498ea1b005279f92044b97af218d1
SavindaJayasekara <thantrige32@gmail.com>
2023-08-23 19:21:18 +0530

    add bo factory !

--- src/main/java/lk/ijse/d24hostalmng/bo/BOFactory.java
+++ src/main/java/lk/ijse/d24hostalmng/bo/BOFactory.java
@@ -1,4 +1,27 @@
 package lk.ijse.d24hostalmng.bo;
 
+import lk.ijse.d24hostalmng.bo.custom.impl.StudentBOImpl;
+
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    public enum BOType{
+        STUDENT,ROOM,RESERVATION
+    }
+
+    public static BOFactory getInstance(){
+        return boFactory == null ? new BOFactory() : boFactory;
+    }
+
+    public <T extends SuperBO>T getBO(BOType boType){
+        switch (boType){
+            case STUDENT:
+                return (T) new StudentBOImpl();
+            default:
+                return null;
+        }
+    }
 }
